0ff52238e6dcfd1e889a34eb4777af6324c618c0
howmehow <howmehow@gmail.com>
2021-05-16 08:26:57 +0200

    Adding howler as a sample player

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+import {Howl, Howler} from 'howler';
+
+const {Howl, Howler} = require('howler');
+
+
+
 const menuToggle = document.querySelector('.toggle')
 const showcase = document.querySelector('.showcase')
 
