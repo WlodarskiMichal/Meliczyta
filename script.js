628a33f9d92ae34df49afed99ea5feded5caeec0
howmehow <howmehow@gmail.com>
2021-05-17 19:31:46 +0200

    Photo styling

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active')
     showcase.classList.toggle('active')
 })
+
+
+var sound = new Howl({
+    src: 'http://prclive1.listenon.in:9960/?fbclid=IwAR1bAO9Hf-yvOGrjKVVdYt0XXnqo85o1G2IXWrzVtjIujOit5JqW7oQUtfI',
+    format: ['mp3', 'aac']
+});sound.play();
