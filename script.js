64aa6c6d6de5d658882fb11b1d1f5b85bf6088d0
howmehow <howmehow@gmail.com>
2021-05-21 22:00:44 +0200

    Moving howler out because it's not working with menu button

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// import {Howl, Howler} from 'howler';
 
-// const {Howl, Howler} = require('howler');
+
 
 
 
@@ -11,9 +10,3 @@ menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active')
     showcase.classList.toggle('active')
 })
-
-
-// var sound = new Howl({
-//     src: 'http://prclive1.listenon.in:9960/?fbclid=IwAR1bAO9Hf-yvOGrjKVVdYt0XXnqo85o1G2IXWrzVtjIujOit5JqW7oQUtfI',
-//     format: ['mp3', 'aac']
-// });sound.play();
