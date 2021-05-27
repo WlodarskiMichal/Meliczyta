440b378ec339e882b933da3eb46c188f89e55ee3
howmehow <howmehow@gmail.com>
2021-05-27 19:52:45 +0200

    Deleting the video

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-
-
-
-
-
 const menuToggle = document.querySelector('.toggle')
 const showcase = document.querySelector('.showcase')
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active')
     showcase.classList.toggle('active')
+})
+
+document.querySelectorAll('.menuItems').forEach(item => {
+    item.addEventListener('click', () => {
+        menuToggle.classList.toggle('active')
+        showcase.classList.toggle('active')
+})
 })
